05dc28c86e448f5f3082c86573a725aba01b65cd
HappyReni <dnjsvlfwo@gmail.com>
2023-08-20 17:50:30 +0900

    [INIT]

---
+++ MathGame/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class GameManager
+    {
+        public GameManager()
+        {
+        }
+
+        private void MainMenu()
+        {
+            Console.WriteLine("".PadRight(24, '='));
+        }
+    }
+
+
+}
