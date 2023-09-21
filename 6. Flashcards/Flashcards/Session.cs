257152be00e706d731060ae8e34d355ddf799bfd
HappyReni <dnjsvlfwo@gmail.com>
2023-09-21 20:41:21 +0900

    Add Session class

---
+++ 6. Flashcards/Flashcards/Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    internal class Session
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Score { get; set; }
+        public int QuestionCount { get; set; }
+
+        public Session(DateTime startTime, DateTime endTime, int score, int questionCount)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            Score = score;
+            QuestionCount = questionCount;
+        }
+    }
+}
