aa7891466ce68df508f6d1a6596742b4afbc531d
HappyReni <dnjsvlfwo@gmail.com>
2023-09-26 21:19:30 +0900

    feat : Add a GetCategories

---
+++ 7. DrinksInfo/DrinksInfo/DrinkService.cs
@@ -0,0 +1,31 @@
+﻿using DrinksInfo.Models;
+using Flashcards;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksInfo
+{
+    public class DrinkService
+    {
+        public void GetCategories()
+        {
+            var client = new RestClient("http://www.thecocktaildb.com/api/json/v1/1/");
+            var request = new RestRequest("list.php?c=list");
+            var response = client.ExecuteAsync(request);
+
+            if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string rawResponse = response.Result.Content;
+                var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
+
+                List<Category> returnedList = serialize.CategoriesList;
+                UI.MakeTable(returnedList, "Categories Menu");
+            }
+        }
+    }
+}
