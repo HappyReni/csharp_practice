916ce28e4ac4c59eec976ac8d7afca386f03a4b6
HappyReni <dnjsvlfwo@gmail.com>
2023-10-06 14:58:58 +0900

    feat: Shift model class and API controller

---
+++ 9. ShfitsLogger/ShiftLoggerAPI/Models/ShiftContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftLoggerAPI.Models
+{
+    public class ShiftContext : DbContext
+    {
+        protected ShiftContext()
+        {
+        }
+
+        public ShiftContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Shift> Shifts { get; set; } = null!;
+    }
+}
