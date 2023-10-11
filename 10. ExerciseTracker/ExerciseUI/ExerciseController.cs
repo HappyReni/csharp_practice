a8d1c4550286d8a525e270bdab027dc42a304343
HappyReni <dnjsvlfwo@gmail.com>
2023-10-11 23:25:55 +0900

    feat : create ExerciseUI project and add GET api function

--- 10. ExerciseTracker/ExerciseUI/ExerciseController.cs
+++ 10. ExerciseTracker/ExerciseUI/ExerciseController.cs
@@ -1,16 +1,25 @@
 ﻿using ExerciseUI.Model;
+using System.Text.Json;
 
 namespace ExerciseUI
 {
     internal class ExerciseController
     {
-        //public static async Task<List<ExerciseModel>> GetExercises()
-        //{
-        //    var endpoint = 
-        //    using(HttpClient client =new HttpClient())
-        //    {
-                
-        //    }
-        //}
+        public static async Task<List<ExerciseModel>> GetExercises()
+        {
+            var endpoint = $"https://localhost:7077/api/ExerciseModels";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(endpoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStreamAsync();
+                    return await JsonSerializer.DeserializeAsync<List<ExerciseModel>>(json);
+                }
+                else throw new Exception("API connection error.");
+            }
+        }
     }
 }
--- 10. ExerciseTracker/ExerciseUI/ExerciseService.cs
+++ 10. ExerciseTracker/ExerciseUI/ExerciseService.cs
@@ -2,5 +2,17 @@
 {
     internal class ExerciseService
     {
+        public static async void ViewAllExercises()
+        {
+            try
+            {
+                var exercises = await ExerciseController.GetExercises();
+                UserInterface.MakeTable(exercises,"All Exercises");
+            }
+            catch (Exception ex)
+            {
+                UserInterface.Write(ex.Message);
+            }
+        }
     }
 }
