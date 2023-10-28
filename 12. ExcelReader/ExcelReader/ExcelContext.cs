5a219be22ae6dd31c5b2edaa690037f64db41937
HappyReni <dnjsvlfwo@gmail.com>
2023-10-28 20:43:39 +0900

    feat : database context

---
+++ 12. ExcelReader/ExcelReader/ExcelContext.cs
@@ -0,0 +1,23 @@
+﻿using ExcelReader.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExcelReader
+{
+    public class ExcelContext : DbContext
+    {
+        protected ExcelContext()
+        {
+        }
+
+        public ExcelContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Excel;Integrated Security=true");
+        }
+
+        public DbSet<ExcelModel> Excels { get; set; } = null;
+    }
+}
---
+++ 12. ExcelReader/ExcelReader/Models/ExcelModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcelReader.Models
+{
+    public class ExcelModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+        public string Address { get; set; }
+    }
+}
