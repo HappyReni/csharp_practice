1db7ef72db0a46838932f69be89a9e5bb794fe73
HappyReni <dnjsvlfwo@gmail.com>
2023-10-29 17:49:04 +0900

    feat : EPPlus package

--- 12. ExcelReader/ExcelReader/Program.cs
+++ 12. ExcelReader/ExcelReader/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using OfficeOpenXml;
+
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+using (var package = new ExcelPackage(new FileInfo("file.xlsx")))
+{
+    var worksheet = package.Workbook.Worksheets["Sheet1"];
+
+    string valueA1 = worksheet.Cells["A1"].Value.ToString();
+    Console.WriteLine(valueA1);
+}
