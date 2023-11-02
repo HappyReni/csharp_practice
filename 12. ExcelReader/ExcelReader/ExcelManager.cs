77235a6c31ecc95747e72c1ba485f217a0440324
HappyReni <dnjsvlfwo@gmail.com>
2023-11-02 18:14:00 +0900

    feat : ExcelService class

--- 12. ExcelReader/ExcelReader/ExcelManager.cs
+++ 12. ExcelReader/ExcelReader/ExcelManager.cs
@@ -5,31 +5,44 @@ namespace ExcelReader
 {
     public class ExcelManager
     {
-        public ExcelManager() 
-        { 
+        public ExcelService _service;
+        private List<ExcelModel> _excelModels;
+        public ExcelManager()
+        {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            Read();
+            _service = new ExcelService(new ExcelContext());
+            _excelModels = Read();
+            Add();
         }
-        public void Read()
+        public List<ExcelModel> Read()
         {
             using (var package = new ExcelPackage(new FileInfo("file.xlsx")))
             {
                 var worksheet = package.Workbook.Worksheets["Sheet1"];
                 var colCount = worksheet.Dimension.End.Column;
                 var rowCount = worksheet.Dimension.End.Row;
-                var cols = worksheet.Columns;
-                for (int i = 2; i < rowCount+1; i++)
+                var res = new List<ExcelModel>();
+
+                for (int i = 2; i < rowCount + 1; i++)
                 {
-                    for (int j =0; j < colCount; j++)
-                    {
-                        char col = (char)('A' + j);
+                    //for (int j =0; j < colCount; j++)
+                    //{
+                    //    char col = (char)('A' + j);
 
-                        Console.WriteLine(worksheet.Cells[$"{col}{i}"].Value.ToString());
-                    }
+                    //    Console.WriteLine(worksheet.Cells[$"{col}{i}"].Value.ToString());
+                    //}
+                    var name = worksheet.Cells[$"A{i}"].Value.ToString();
+                    var age = Int32.Parse(worksheet.Cells[$"B{i}"].Value.ToString());
+                    var job = worksheet.Cells[$"C{i}"].Value.ToString();
+                    var address = worksheet.Cells[$"D{i}"].Value.ToString();
+                    res.Add(new ExcelModel { Name = name, Age = age, Job = job, Address = address });
                 }
-                //string valueA1 = worksheet.Cells["A1"].Value.ToString();
+                return res;
             }
-
+        }
+        public void Add()
+        {
+            _service.Create(_excelModels);
         }
     }
 }
---
+++ 12. ExcelReader/ExcelReader/ExcelService.cs
@@ -0,0 +1,20 @@
+﻿using ExcelReader.Models;
+
+namespace ExcelReader
+{
+    public class ExcelService
+    {
+        public ExcelContext _context;
+
+        public ExcelService(ExcelContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(List<ExcelModel> models)
+        {
+            _context.Excels.AddRange(models);
+            _context.SaveChanges();
+        }
+    }
+}
