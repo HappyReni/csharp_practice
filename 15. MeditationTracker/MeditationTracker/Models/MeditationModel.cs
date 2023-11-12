00859d63272e3b34e7ad282a7471983e7185ce6a
HappyReni <dnjsvlfwo@gmail.com>
2023-11-12 17:19:43 +0900

    init

---
+++ 15. MeditationTracker/MeditationTracker/Models/MeditationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MeditationTracker.Models
+{
+    public class MeditationModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+    }
+}
