d8410baae3c65a4acee6cd87e4bc3c225e8db439
HappyReni <dnjsvlfwo@gmail.com>
2023-11-12 18:06:29 +0900

    feat : CRUD functions

--- 15. MeditationTracker/MeditationTracker/Models/MeditationModel.cs
+++ 15. MeditationTracker/MeditationTracker/Models/MeditationModel.cs
@@ -1,8 +1,11 @@
-﻿namespace MeditationTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeditationTracker.Models
 {
     public class MeditationModel
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         public int Duration { get; set; }
     }
--- 15. MeditationTracker/MeditationTracker/Pages/Index.cshtml.cs
+++ 15. MeditationTracker/MeditationTracker/Pages/Index.cshtml.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeditationTracker.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using System.Globalization;
 
 namespace MeditationTracker.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        public List<MeditationModel> Records { get; set; }
+        public IndexModel(IConfiguration configuration)
         {
-            _logger = logger;
+            _configuration = configuration;
         }
-
         public void OnGet()
         {
+            Records = GetAllRecords();
+        }
+        private List<MeditationModel> GetAllRecords()
+        {
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $"SELECT * FROM Meditation ";
 
+                var tableData = new List<MeditationModel>();
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tableData.Add(
+                    new MeditationModel
+                    {
+                        Id = reader.GetInt32(0),
+                        Date = DateTime.Parse(reader.GetString(1), CultureInfo.CurrentUICulture.DateTimeFormat),
+                        Duration = reader.GetInt32(2)
+                    });
+                }
+
+                return tableData;
+            }
         }
     }
 }
